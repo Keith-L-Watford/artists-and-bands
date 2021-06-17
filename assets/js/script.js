f4f2cd7702f934e5f75e34a6d10c6823366e76b4
Keith Watford <keith.l.watford@gmail.com>
2021-06-17 09:49:20 -0400

    Introducing for loops. added a drill to artist/event name.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 var userArtist = "";
 var userZipCode = "";
 
-// var testZipCode = 19107;
-var testKeyword = "jackson";
+var testZipCode = 19107;
+var testKeyword = "key";
 var ticketMasterKey = "&apikey=uuD6hzdyqgKtPGThGyiUbWE3EVpVuaAc";
 var tickerMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + testKeyword + ticketMasterKey;
 // + "&postalCode=" + testZipCode
@@ -11,6 +11,7 @@ var tickerMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?key
 // test zipcode is Center City, Philadelphia
 // temporailly defining the searched keyword until its connected to an input in HTML
 var test = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=jackson&apikey=uuD6hzdyqgKtPGThGyiUbWE3EVpVuaAc"
+
 // var testWithZip = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=jackson&postalCode=19107&apikey=uuD6hzdyqgKtPGThGyiUbWE3EVpVuaAc"
 
 
@@ -35,26 +36,38 @@ fetch(tickerMasterURL)
 })
 .then(function (data) {
         console.log(data)
-      
 
-        // // link to artists wikipedia
-        console.log(data._embedded.events[0]._embedded.attractions[0].externalLinks.wiki[0].url);
+            var i;
+            for (i = 0; i < 10; i++) {
+                
+                console.log(data._embedded.events[i].name);
+                // console.log(data._embedded.events[i]._embedded.attractions[0].url);
+            }
 
-        // // link to artists last.fm
-        console.log(data._embedded.events[0]._embedded.attractions[0].externalLinks.lastfm[0].url);
+        // // // Artist Info
+        //     // Aritst/event name
+        // // console.log(data._embedded.events[0].name);
 
         // // link to ticket masters artist profile
-        console.log(data._embedded.events[0]._embedded.attractions[0].url);
+        // console.log(data._embedded.events[0]._embedded.attractions[0].url);
 
-        // // venue info
-        //     // Street Address
-        console.log(data._embedded.events[0]._embedded.venues[0].address.line1);
-        //     // City
-        console.log(data._embedded.events[0]._embedded.venues[0].city.name);
-        //     // State (abreviated - can replace 'stateCode' with 'name' to get the full name of the state.)
-        console.log(data._embedded.events[0]._embedded.venues[0].state.stateCode);
-        //     // Zip code
-        console.log(data._embedded.events[0]._embedded.venues[0].postalCode);
-        //     // Venue website
-        console.log(data._embedded.events[0]._embedded.venues[0].url);
+        // // // venue info
+        // //     // Street Address
+        // console.log(data._embedded.events[0]._embedded.venues[0].address.line1);
+        // //     // City
+        // console.log(data._embedded.events[0]._embedded.venues[0].city.name);
+        // //     // State (abreviated - can replace 'stateCode' with 'name' to get the full name of the state.)
+        // console.log(data._embedded.events[0]._embedded.venues[0].state.stateCode);
+        // //     // Zip code
+        // console.log(data._embedded.events[0]._embedded.venues[0].postalCode);
+        // //     // Venue website
+        // console.log(data._embedded.events[0]._embedded.venues[0].url);
+
+
+
+        // Possible external links, - note not all artists/events have external links
+            // link to artists wikipedia
+        // console.log(data._embedded.events[0]._embedded.attractions[0].externalLinks.wiki[0].url);
+            // link to artists last.fm
+        // console.log(data._embedded.events[0]._embedded.attractions[0].externalLinks.lastfm[0].url);
     });
