ced2faa3fe4bf5788740c65b7dfb2d0770898f6c
Keith Watford <keith.l.watford@gmail.com>
2021-06-16 20:22:56 -0400

    Drilling into the API and creating console logs of the relevant info

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,15 @@ var ticketMasterKey = "&apikey=uuD6hzdyqgKtPGThGyiUbWE3EVpVuaAc";
 var tickerMasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + testKeyword + ticketMasterKey;
 
 // + "&postalCode=" + testZipCode
-
+// var testZipCode = 19107;
 var testKeyword = "jackson";
-var testZipCode = 19107;
+
 // test zipcode is Center City, Philadelphia
 // temporailly defining the searched keyword until its connected to an input in HTML
 var test = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=jackson&apikey=uuD6hzdyqgKtPGThGyiUbWE3EVpVuaAc"
 
 console.log(test);
+
 
 
 
@@ -33,4 +34,20 @@ fetch(test)
 })
 .then(function (data) {
         console.log(data)
+        console.log(data._embedded.events[0].url);
+
+        // link to artists wikipedia
+        console.log(data._embedded.attractions[0].externalLinks.wiki[0].url);
+
+        // link to artists last.fm
+        console.log(data._embedded.attractions[0].externalLinks.lastfm[0].url);
+
+        // link to ticket masters artist profile
+        console.log(data._embedded.attractions[0].url);
+
+        // venue info
+        console.log();
+        console.log();
+        console.log();
+        console.log();
     });
